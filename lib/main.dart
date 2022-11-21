4fd96f1cdf35977f669074a689bc4ae84efaa9c5
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-11-21 19:31:33 +0500

    Added dependence: flutter_svg: ^1.1.6
    Created dart files:
    constants.dart
    category_card.dart
    bottom_nav_bar.dart
    
    Created folders:
    assets/
          fonts/
          icons/
          images/
    
    Added images: .svg's, .jpg, .png.
    
    Added fonts: Cairo-Black.ttf
                 Cairo-Bold.ttf
                 Cairo-Regular.ttf
                 Cairo-SemiBold.ttf

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:daily_exercises_app_ui/constants.dart';
+import 'package:daily_exercises_app_ui/widgets/bottom_nav_bar.dart';
+import 'package:daily_exercises_app_ui/widgets/category_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,57 +14,118 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Daily Exercises App UI',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: "Cairo",
+        scaffoldBackgroundColor: kBackgroundColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * 0.45,
+            decoration: BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20.0,
+                top: 10.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 52,
+                      width: 52,
+                      decoration: const BoxDecoration(
+                        color: Color(0xFFF2BEA1),
+                        shape: BoxShape.circle,
+                      ),
+                      child: SvgPicture.asset("assets/icons/menu.svg"),
+                    ),
+                  ),
+                  Text(
+                    "Good Morning \nKety",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline4!
+                        .copyWith(fontWeight: FontWeight.w900),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 30),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(29.5)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Search',
+                        icon: SvgPicture.asset("assets/icons/search.svg"),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.85,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20,
+                      children: [
+                        CategoryCard(
+                          title: 'Diet Recommendation',
+                          svgSrc: 'assets/icons/Hamburger.svg',
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: 'Kegel Exercises',
+                          svgSrc: 'assets/icons/Excrecises.svg',
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: 'Meditation',
+                          svgSrc: 'assets/icons/Meditation.svg',
+                          press: () {},
+                        ),
+                        CategoryCard(
+                          title: 'Yoga',
+                          svgSrc: 'assets/icons/yoga.svg',
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
